457cdb0cc3fac55abf204e245ecc19d7312a53e2
guanglei1wang <guanglei.wgl13@gmail.com>
2019-10-10 17:56:29 +0800

    add configuration file

---
+++ cmake/FindCOINUTILS.cmake
@@ -0,0 +1,27 @@
+set(COINUTILS_ROOT_DIR "$ENV{COINUTILS_ROOT_DIR}" CACHE PATH "COINUTILS root directory.")
+message("Looking for COINUTILS in ${COINUTILS_ROOT_DIR} folder")
+
+find_path(COINUTILS_INCLUDE_DIR  
+  NAMES CoinPragma.hpp 
+  HINTS ${COINUTILS_ROOT_DIR}/include/coinutils/coin/
+)
+
+find_library(COINUTILS_LIBRARY
+  NAMES libCoinUtils.dylib
+  HINTS ${COINUTILS_ROOT_DIR}/lib/coinutils/
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(COINUTILS DEFAULT_MSG COINUTILS_LIBRARY COINUTILS_INCLUDE_DIR)
+
+if(COINUTILS_FOUND)
+    message("—- Found COINUTILS under ${COINUTILS_INCLUDE_DIR}")
+    set(COINUTILS_INCLUDE_DIRS ${COINUTILS_INCLUDE_DIR})
+    set(COINUTILS_LIBRARIES ${COINUTILS_LIBRARY}  ${COINUTILS_ROOT_DIR}/lib)
+    link_directories(${COINUTILS_ROOT_DIR}/lib/)
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        set(COINUTILS_LIBRARIES "${COINUTILS_LIBRARIES};m;pthread")
+    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif(COINUTILS_FOUND)
+
+mark_as_advanced(COINUTILS_LIBRARY COINUTILS_INCLUDE_DIR)
--- examples/Polar_test.cpp
+++ examples/Polar_test.cpp
@@ -5,7 +5,43 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "ClpSimplex.hpp"
 
 TEST_CASE("test clp"){
-
+    ClpSimplex  model;
+    int status;
+    // Keep names
+    status = model.readMps(string(prj_dir)+"hello.mps", true);
+    if (status)
+        exit(10);
+    
+    int numberColumns = model.numberColumns();
+    int numberRows = model.numberRows();
+    
+    if (numberColumns > 80 || numberRows > 80) {
+        printf("model too large\n");
+        exit(11);
+    }
+    printf("This prints x wherever a non-zero element exists in the matrix.\n\n\n");
+    
+    char x[81];
+    
+    int iRow;
+    // get row copy
+    CoinPackedMatrix rowCopy = *model.matrix();
+    rowCopy.reverseOrdering();
+    const int * column = rowCopy.getIndices();
+    const int * rowLength = rowCopy.getVectorLengths();
+    const CoinBigIndex * rowStart = rowCopy.getVectorStarts();
+    
+    x[numberColumns] = '\0';
+    for (iRow = 0; iRow < numberRows; iRow++) {
+        memset(x, ' ', numberColumns);
+        for (CoinBigIndex k = rowStart[iRow]; k < rowStart[iRow] + rowLength[iRow]; k++) {
+            int iColumn = column[k];
+            x[iColumn] = 'x';
+        }
+        printf("%s\n", x);
+    }
+    printf("\n\n");
 }
