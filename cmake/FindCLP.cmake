00ff8f395b169cd95cdd41dd3b11fc1d4f2e836a
guanglei1wang <guanglei.wgl13@gmail.com>
2019-10-10 19:51:11 +0800

    remove useless libs in CLP and try travis

--- cmake/FindCLP.cmake
+++ cmake/FindCLP.cmake
@@ -10,22 +10,18 @@ find_library(CLP_LIBRARY
     NAMES libClp.dylib
     HINTS ${CLP_ROOT_DIR}/lib/
     )
-find_library(UTILS_LIBRARY
-  NAMES libClpSolver..dylib
-  HINTS ${CLP_ROOT_DIR}/lib/
-  )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CLP DEFAULT_MSG UTILS_LIBRARY CLP_LIBRARY CLP_INCLUDE_DIR)
+find_package_handle_standard_args(CLP DEFAULT_MSG CLP_LIBRARY CLP_INCLUDE_DIR)
 
 if(CLP_FOUND)
     message("—- Found CLP under ${CLP_INCLUDE_DIR}")
     set(CLP_INCLUDE_DIRS ${CLP_INCLUDE_DIR})
-    set(CLP_LIBRARIES ${CLP_LIBRARY} ${UTILS_LIBRARY} ${CLP_ROOT_DIR}/lib)
+    set(CLP_LIBRARIES ${CLP_LIBRARY}  ${CLP_ROOT_DIR}/lib)
     link_directories(${CLP_ROOT_DIR}/lib/)
     if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
         set(CLP_LIBRARIES "${CLP_LIBRARIES};m;pthread")
     endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 endif(CLP_FOUND)
 
-mark_as_advanced(CLP_LIBRARY UTILS_LIBRARY CLP_INCLUDE_DIR)
+mark_as_advanced(CLP_LIBRARY CLP_INCLUDE_DIR)
