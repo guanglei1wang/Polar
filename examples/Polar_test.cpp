33c7918c48064b107ff2c72e856454e68177244c
guanglei1wang <guanglei.wgl13@gmail.com>
2019-10-11 10:49:44 +0800

    test osi and add cgl test data

--- examples/Polar_test.cpp
+++ examples/Polar_test.cpp
@@ -16,9 +16,15 @@
 
 #include "doctest.h"
 #include "Config.h"
+
 #include "ClpSimplex.hpp"
 
-TEST_CASE("test clp"){
+#include "OsiSolverInterface.hpp"
+#include "OsiClpSolverInterface.hpp"
+
+
+
+TEST_CASE("test clp simplex"){
     ClpSimplex  model;
     int status;
     std::string mpsfile=prj_dir"/data/hello.mps";
@@ -58,5 +64,243 @@ TEST_CASE("test clp"){
     printf("\n\n");
 }
 
+
+TEST_CASE("test Osi"){
+    // Create a problem pointer.  We use the base class here.
+    OsiSolverInterface *si;
+    
+    // When we instantiate the object, we need a specific derived class.
+    si = new OsiClpSolverInterface; 
+    
+    // Read in an mps file.  This one's from the MIPLIB library.
+    std::string mpsfile=prj_dir"/data/Sample/p0033.mps";
+
+    if (si->readMps(mpsfile.c_str()) <0){
+        exit(1);
+    }
+
+    // Display some information about the instance
+    int nrows = si->getNumRows();
+    int ncols = si->getNumCols();
+    int nelem = si->getNumElements();
+    std::cout << "This problem has " << nrows << " rows, "
+    << ncols << " columns, and " << nelem << " nonzeros."
+    << std::endl;
+    
+    double const * upper_bounds = si->getColUpper();
+    std::cout << "The upper bound on the first column is " << upper_bounds[0]
+    << std::endl;
+    // All information about the instance is available with similar methods
+    
+    
+    // Before solving, indicate some parameters
+    si->setIntParam( OsiMaxNumIteration, 10);
+    si->setDblParam( OsiPrimalTolerance, 0.001 );
+    
+    // Can also read parameters
+    std::string solver;
+    si->getStrParam( OsiSolverName, solver );
+    std::cout << "About to solve with: " << solver << std::endl;
+    
+    // Solve the (relaxation of the) problem
+    si->initialSolve();
+    
+    // Check the solution
+    if ( si->isProvenOptimal() ) {
+        std::cout << "Found optimal solution!" << std::endl;
+        std::cout << "Objective value is " << si->getObjValue() << std::endl;
+        
+        // Examine solution
+        int n = si->getNumCols();
+        const double *solution;
+        solution = si->getColSolution();
+        
+        std::cout << "Solution: ";
+        for (int i = 0; i < n; i++)
+            std::cout << solution[i] << " ";
+        std::cout << std::endl;
+        
+        std::cout << "It took " << si->getIterationCount() << " iterations"
+        << " to solve." << std::endl;
+    } else {
+        std::cout << "Didn't find optimal solution." << std::endl;
+        
+        // Check other status functions.  What happened?
+        if (si->isProvenPrimalInfeasible())
+            std::cout << "Problem is proven to be infeasible." << std::endl;
+        if (si->isProvenDualInfeasible())
+            std::cout << "Problem is proven dual infeasible." << std::endl;
+        if (si->isIterationLimitReached())
+            std::cout << "Reached iteration limit." << std::endl;
+    }
+}
+
+
 TEST_CASE("test cgl"){
+    char *f_name_lp, *last_dot_pos, f_name[256], *f_name_pos;
+    int i, ncol;
+    std::string mpsfile=prj_dir"/data/Sample/p0033.mps";
+
+    f_name_lp = strdup(mpsfile.c_str());
+    f_name_pos = strrchr(f_name_lp, '/');
+    if(f_name_pos != NULL) {
+        strcpy(f_name, &(f_name_pos[1]));
+    }
+    else {
+        strcpy(f_name, f_name_lp);
+    }
+    last_dot_pos = strrchr(f_name, '.');
+    if(last_dot_pos != NULL) {
+        last_dot_pos = '\0';
+    }
+    
+    // Do for both RedSplit and RedSplit2
+    {
+        OsiClpSolverInterface *clp = new OsiClpSolverInterface;
+        clp->messageHandler()->setLogLevel(0);
+        if(strcmp(&(f_name_lp[strlen(f_name_lp)-3]), ".lp") == 0) {
+            clp->readLp(f_name_lp);
+        }
+        else {
+            if(strcmp(&(f_name_lp[strlen(f_name_lp)-4]), ".mps") == 0) {
+                clp->readMps(f_name_lp);
+            }
+            else {
+                printf("### ERROR: unrecognized file type\n");
+                exit(1);
+            }
+        }
+        ncol = clp->getNumCols();
+        clp->initialSolve();
+        
+        printf("LP value: %12.2f\n", clp->getObjValue());
+        
+        OsiCuts cuts;
+        
+        // Define parameters for CglRedSplit generator
+        CglParam cpar;
+        cpar.setMAX_SUPPORT(ncol+1);
+        CglRedSplitParam rspar(cpar);
+        
+        // Create a cut generator with the given parameters
+        CglRedSplit cutGen(rspar);
+        
+        char *colType = new char[ncol];
+        for(i=0; i<ncol; i++) {
+            if(clp->isContinuous(i)) {
+                colType[i] = 'C';
+            }
+            else {
+                colType[i] = 'I';
+            }
+        }
+        
+        int round, max_rounds = 10;
+        for(round=0; round<max_rounds; round++) {
+            cutGen.generateCuts(*clp, cuts);
+            
+            int ncuts = cuts.sizeRowCuts();
+            
+            const OsiRowCut **newRowCuts = new const OsiRowCut * [ncuts];
+            for(i=0; i<ncuts; i++) {
+                newRowCuts[i] = &cuts.rowCut(i);
+            }
+            clp->applyRowCuts(ncuts, newRowCuts);
+            delete[] newRowCuts;
+            
+            printf("round %4d: %4d generated cuts  new objective value: %12.2f\n",
+                   round, ncuts, clp->getObjValue());
+            
+            clp->resolve();
+            
+            if(clp->isAbandoned()) {
+                printf("###ERROR: Numerical difficulties in Solver\n");
+                exit(1);
+            }
+            
+            if(clp->isProvenPrimalInfeasible()) {
+                printf("### WARNING: Problem is infeasible\n");
+                exit(1);
+            }
+        }
+        
+        delete clp;
+        delete[] colType;
+    }
+    {
+        OsiClpSolverInterface *clp = new OsiClpSolverInterface;
+        clp->messageHandler()->setLogLevel(0);
+        if(strcmp(&(f_name_lp[strlen(f_name_lp)-3]), ".lp") == 0) {
+            clp->readLp(f_name_lp);
+        }
+        else {
+            if(strcmp(&(f_name_lp[strlen(f_name_lp)-4]), ".mps") == 0) {
+                clp->readMps(f_name_lp);
+            }
+            else {
+                printf("### ERROR: unrecognized file type\n");
+                exit(1);
+            }
+        }
+        ncol = clp->getNumCols();
+        clp->initialSolve();
+        
+        printf("LP value: %12.2f\n", clp->getObjValue());
+        
+        OsiCuts cuts;
+        
+        // Define parameters for CglRedSplit2 generator
+        CglParam cpar;
+        cpar.setMAX_SUPPORT(ncol+1);
+        CglRedSplit2Param rspar(cpar);
+        
+        // Create a cut generator with the given parameters
+        CglRedSplit2 cutGen(rspar);
+        
+        char *colType = new char[ncol];
+        for(i=0; i<ncol; i++) {
+            if(clp->isContinuous(i)) {
+                colType[i] = 'C';
+            }
+            else {
+                colType[i] = 'I';
+            }
+        }
+        
+        int round, max_rounds = 10;
+        for(round=0; round<max_rounds; round++) {
+            cutGen.generateCuts(*clp, cuts);
+            
+            int ncuts = cuts.sizeRowCuts();
+            
+            const OsiRowCut **newRowCuts = new const OsiRowCut * [ncuts];
+            for(i=0; i<ncuts; i++) {
+                newRowCuts[i] = &cuts.rowCut(i);
+            }
+            clp->applyRowCuts(ncuts, newRowCuts);
+            delete[] newRowCuts;
+            
+            printf("round %4d: %4d generated cuts  new objective value: %12.2f\n",
+                   round, ncuts, clp->getObjValue());
+            
+            clp->resolve();
+            
+            if(clp->isAbandoned()) {
+                printf("###ERROR: Numerical difficulties in Solver\n");
+                exit(1);
+            }
+            
+            if(clp->isProvenPrimalInfeasible()) {
+                printf("### WARNING: Problem is infeasible\n");
+                exit(1);
+            }
+        }
+        
+        delete clp;
+        delete[] colType;
+    }
+    free(f_name_lp);
+}
+
+TEST_CASE(""){
 }
